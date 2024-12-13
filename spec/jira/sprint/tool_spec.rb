f11064459d6ea68314f0da33dae8be905ba071bc
Christophe Broult <cbroult@yahoo.com>
2024-12-13 14:13:06 +0100

    Remove rspec initial failing test

--- spec/jira/sprint/tool_spec.rb
+++ spec/jira/sprint/tool_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Jira::Sprint::Tool do
   it "has a version number" do
     expect(Jira::Sprint::Tool::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
