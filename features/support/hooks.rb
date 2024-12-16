1cdbd552751ff42c7647d41c8fa0e837e9f85153
Christophe Broult <cbroult@yahoo.com>
2024-12-16 13:03:28 +0100

    Removed duplication by using Jira::Sprint::Tool instead of the Jira client

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,18 +2,12 @@
 
 require "jira-ruby"
 
-jira_options = {
-  site: "https://cbroult.atlassian.net/",
-  context_path: "",
-  auth_type: :basic,
-  username: "cbroult@yahoo.com",
-  password: ENV.fetch("JIRA_API_TOKEN").chomp
-}
+require "jira/sprint/tool"
 
 Before do
-  @jira_client = JIRA::Client.new(jira_options)
-  board_mame = "JST - Self-Test Board - Dev"
-  @board = @jira_client.Board.all.find { |board| board.name == board_mame } || raise("#{board_mame}: Board not found")
+  @jira_sprint_tool = Jira::Sprint::Tool.new
+  @jira_client = @jira_sprint_tool.jira_client
+  @board = @jira_sprint_tool.board
 
   remove_existing_sprints(@board)
 end
