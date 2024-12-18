d3ff54105808326de3facd815857d5036e3ee5ac
Christophe Broult <cbroult@yahoo.com>
2024-12-18 22:11:51 +0100

    Reducing the length of methods

--- lib/jira/sprint/tool.rb
+++ lib/jira/sprint/tool.rb
@@ -5,7 +5,6 @@ require "date"
 require "active_support/core_ext/numeric/time"
 require "active_support/core_ext/date/calculations"
 require "jira-ruby"
-require "pp"
 require_relative "tool/version"
 require_relative "tool/sprint_controller"
 require_relative "tool/sprint_generator"
@@ -34,41 +33,13 @@ module Jira
         jira_client.Board.all
       end
 
-      module SprintState
-        ACTIVE = "active"
-        FUTURE = "future"
-        CLOSED = "closed"
-      end
-
-      # TODO: - write unit tests
-      def transition_sprint_state(name:, desired_state:)
-        sprint_to_update = fetch_sprint(name)
-
-        log.debug { "sprint_to_update = #{sprint_to_update}, desired_state = #{desired_state}" }
-
-        new_state =
-          case sprint_to_update.state
-          when desired_state
-            return
-          when SprintState::FUTURE
-            SprintState::ACTIVE
-          when SprintState::ACTIVE
-            SprintState::CLOSED
-          else
-            raise "unexpected sprint state = #{sprint_to_update.state}. Was trying to transition to #{desired_state}."
-          end
-
-        update_sprint_state(sprint: sprint_to_update, new_state: new_state)
-        transition_sprint_state(name: name, desired_state: desired_state)
-      end
-
       def fetch_sprint(sprint_name)
         board.sprints.find { |sprint| sprint.name == sprint_name } or
           raise KeyError, "Sprint '#{sprint_name}' not found for #{board.name}!" # TODO: - test this condition
       end
 
       def create_sprint(name:, start: Time.now.utc.iso8601, length_in_days: 14, goal: nil, state: "future")
-        create_future_sprint(goal, length_in_days, name, start)
+        create_future_sprint(name, start, length_in_days, goal)
 
         transition_sprint_state(name: name, desired_state: state)
       end
@@ -103,22 +74,45 @@ module Jira
         @sprint_generator ||= SprintGenerator.new
       end
 
+      module SprintState
+        ACTIVE = "active"
+        FUTURE = "future"
+        CLOSED = "closed"
+      end
+
+      STATE_TRANSITIONS =
+        {
+          SprintState::FUTURE => SprintState::ACTIVE,
+          SprintState::ACTIVE => SprintState::CLOSED
+        }.freeze
+
+      # TODO: - write unit tests
+      # TODO - fix infinite loop in case of invalid/in-existing state
+      def transition_sprint_state(name:, desired_state:)
+        sprint_to_update = fetch_sprint(name)
+
+        log.debug { "sprint_to_update = #{sprint_to_update}, desired_state = #{desired_state}" }
+
+        current_state = sprint_to_update.state
+
+        return if current_state == desired_state
+
+        update_sprint_state(sprint: sprint_to_update, new_state: STATE_TRANSITIONS.fetch(current_state))
+
+        transition_sprint_state(name: name, desired_state: desired_state)
+      end
+
       private
 
-      def create_future_sprint(goal, length_in_days, name, start)
+      def create_future_sprint(name, start, length_in_days, goal)
         start_date = Time.parse(start)
         end_date = start_date + length_in_days.days
 
         response = jira_client.post(
-          "/rest/agile/1.0/sprint",
-          {
-            originBoardId: board.id,
-            name: name,
-            startDate: start_date.utc.iso8601,
-            endDate: end_date.utc.iso8601,
-            goal: goal
-          }.to_json,
-          { "Content-Type" => "application/json" }
+          *build_request_args(
+            "/rest/agile/1.0/sprint",
+            build_create_future_sprint_payload(name, end_date, start_date, goal)
+          )
         )
 
         if response.code.to_i == 201
@@ -128,10 +122,29 @@ module Jira
           log.error { error_message }
           raise error_message
         end
-      rescue JIRA::HTTPError => e
-        puts "HTTP error occurred: #{e.response.code} - #{e.response.body}"
-      rescue StandardError => e
-        puts "An error occurred: #{e.message}"
+      end
+
+      def build_create_future_sprint_payload(name, end_date, start_date, goal)
+        {
+          originBoardId: board.id,
+          name: name,
+          startDate: start_date.utc.iso8601,
+          endDate: end_date.utc.iso8601,
+          goal: goal
+        }
+      end
+
+      def update_sprint_state(sprint:, new_state:)
+        response = jira_client.put(*build_request_args("/rest/agile/1.0/sprint/#{sprint.id}",
+                                                       build_update_sprint_state_payload(new_state, sprint)))
+
+        if response.code.to_i == 200
+          log.debug { "Sprint state updated successfully: #{response.body}" }
+        else
+          error_message = "Error updating sprint state: #{response.code} - #{response.body}"
+          log.error { error_message }
+          raise error_message
+        end
       end
 
       ATTRIBUTES_TO_INCLUDE_FOR_STATE_UPDATE = %i[
@@ -143,32 +156,21 @@ module Jira
         originBoardId
       ].freeze
 
-      def update_sprint_state(sprint:, new_state:)
+      def build_update_sprint_state_payload(new_state, sprint)
         attributes = sprint.attrs.symbolize_keys
 
-        payload = ATTRIBUTES_TO_INCLUDE_FOR_STATE_UPDATE.each_with_object({}) do |key, result|
+        ATTRIBUTES_TO_INCLUDE_FOR_STATE_UPDATE.each_with_object({}) do |key, result|
           result[key] = attributes[key]
         end
-                    .merge({ state: new_state })
+          .merge({ state: new_state })
+      end
 
-        put_args = [
-          "/rest/agile/1.0/sprint/#{sprint.id}",
+      def build_request_args(request_url, payload)
+        [
+          request_url,
           payload.to_json,
           { "Content-Type" => "application/json" }
         ]
-
-        response = jira_client.put(*put_args)
-
-        if response.code.to_i == 200
-          log.debug { "Sprint state updated successfully: #{response.body}" }
-        else
-          error_message = "Error updating sprint state: #{response.code} - #{response.body}"
-          log.error { error_message }
-          raise error_message
-        end
-      rescue JIRA::HTTPError => e
-        log.error { e.message }
-        raise e
       end
 
       def fetch_corresponding_environment_variable
