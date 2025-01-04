3f031a9312cc7bec0986df1a99bb8816a9de5bbd
Christophe Broult <cbroult@yahoo.com>
2025-01-04 11:55:51 +0100

    Cleaned TeamSprintPrefixMapper spec further

--- spec/jira/auto/tool/team_sprint_prefix_mapper_spec.rb
+++ spec/jira/auto/tool/team_sprint_prefix_mapper_spec.rb
@@ -6,14 +6,16 @@ module Jira
   module Auto
     class Tool
       class TeamSprintPrefixMapper
-        # rubocop:disable RSpec/MultipleMemoizedHelpers
         RSpec.describe TeamSprintPrefixMapper do
           let(:mapper) do
             described_class.new(teams, unclosed_sprint_prefixes)
           end
 
-          let(:team_names) { ["A16 CRM", "A16 Logistic", "A16 Platform", "A32 64 Sys-Team"] }
-          let(:teams) { team_names.collect { |name| instance_double(Team, name: name) } }
+          let(:teams) do
+            ["A16 CRM", "A16 Logistic", "A16 Platform", "A32 64 Sys-Team"].collect do |name|
+              instance_double(Team, name: name)
+            end
+          end
 
           let(:expected_team_sprint_prefix_mappings) do
             {
@@ -23,27 +25,10 @@ module Jira
             }
           end
 
-          let(:sprint_controller) do
-            instance_double(SprintController,
-                            unclosed_sprint_prefixes: unclosed_sprint_prefixes)
-          end
-
-          let(:unclosed_sprint_prefix_names) { %w[ART-16_CRM ART-16_Platform ART-32-64_Sys-Team] }
-
           let(:unclosed_sprint_prefixes) do
-            unclosed_sprint_prefix_names.collect { |name| instance_double(Sprint::Prefix, name: name) }
-          end
-
-          let(:tool) do
-            instance_double(Tool,
-                            implementation_team_field: team_field,
-                            sprint_controller: sprint_controller,
-                            teams: teams)
-          end
-          let(:team_field) { instance_double(Field, name: "team_name") }
-
-          def build_sprint(name)
-            instance_double(Sprint, name: name, name_prefix: name.sub(/_[^_]+$/, ""))
+            %w[ART-16_CRM ART-16_Platform ART-32-64_Sys-Team].collect do |name|
+              instance_double(Sprint::Prefix, name: name)
+            end
           end
 
           describe "#sprint_prefixes" do
@@ -66,7 +51,7 @@ module Jira
               expect { mapper.map_prefix_name_to_team_name("team-unrelated-prefix") }
                 .to raise_error(NoMatchingTeamError,
                                 /No\smatching\steam\sfound\sfor\sprefix\s'team-unrelated-prefix'\sin\s
-                                  #{Regexp.escape(team_names.inspect)}/x)
+                                  #{Regexp.escape(teams.collect(&:name).inspect)}/x)
             end
           end
 
@@ -93,7 +78,6 @@ module Jira
             end
           end
         end
-        # rubocop:enable RSpec/MultipleMemoizedHelpers
       end
     end
   end
