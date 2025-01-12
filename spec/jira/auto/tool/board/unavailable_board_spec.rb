425a12567877d72da89c4043025ed82eb3ff9841
Christophe Broult <cbroult@yahoo.com>
2025-01-12 15:15:42 +0100

    Updated the needed Gem versions

---
+++ spec/jira/auto/tool/board/unavailable_board_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "jira/auto/tool/board/unavailable_board"
+
+module Jira
+  module Auto
+    class Tool
+      class Board
+        class UnavailableBoard < Board
+          RSpec.describe UnavailableBoard do
+            let(:jira_client) { instance_double(JIRA::Client) }
+            let(:tool) { instance_double(Tool, jira_client: jira_client) }
+
+            let(:unavailable_board) { described_class.new(tool, 64) }
+
+            it { expect(board.name).to eq("N/A") }
+
+            it { expect(board.url).to eq("N/A") }
+
+            it { expect(board.id).to eq(64) }
+
+            describe "#<=>" do
+              def new_board(id)
+                described_class.new(tool, id)
+              end
+
+              let(:board_with_same_id) { new_board(4) }
+
+              it { expect(board <=> board_with_same_id).to eq(0) }
+              it { expect(new_board(1) <=> board).to eq(-1) }
+              it { expect(new_board(5) <=> board).to eq(1) }
+            end
+          end
+        end
+      end
+    end
+  end
+end
