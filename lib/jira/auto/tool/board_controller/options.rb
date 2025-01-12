19228acf9b5f31532e2e09d9666967f214e0bc60
Christophe Broult <cbroult@yahoo.com>
2025-01-12 17:22:25 +0100

    Added support for optional/missing sprint dates and missing boards

--- lib/jira/auto/tool/board_controller/options.rb
+++ lib/jira/auto/tool/board_controller/options.rb
@@ -9,7 +9,7 @@ module Jira
         class Options
           def self.add(tool, parser)
             parser.on("--board-list", "List boards. The output can be controlled via the " \
-              "#{Tool::Environment::JIRA_BOARD_NAME_REGEX} environment variable") do
+                                      "#{Tool::Environment::JIRA_BOARD_NAME_REGEX} environment variable") do
               tool.board_controller.list_boards
             end
           end
