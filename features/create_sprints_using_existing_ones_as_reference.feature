9ea417a380025f0f8b0f71787ab64a5221dfea88
Christophe Broult <cbroult@yahoo.com>
2025-05-09 10:55:25 +0200

    Prevent creation of duplicate or anterior sprints.
    Updated sprint creation logic to skip sprints that are duplicative or anterior to the last sprint of a prefix. Adjusted tests and scenarios to ensure accurate behavior and no unintended sprint creation.

--- features/create_sprints_using_existing_ones_as_reference.feature
+++ features/create_sprints_using_existing_ones_as_reference.feature
@@ -5,14 +5,14 @@ Feature: Add sprints using existing ones as reference
 
   Background:
     Given a Jira Scrum board
-    And the board only has the following sprints:
+
+  Scenario: Add several sprints using existing sprint prefixes
+    Given the board only has the following sprints:
       | comment                 | name                   | length | start_date              | state  |
       | none added since closed | Food_Supply_25.1.3     | 2-week | 2025-02-01 11:00:00 UTC | closed |
       | "sprints                | Food_Delivery_25.1.4   | 4-day  | 2025-02-01 11:00:00 UTC | future |
       | expected to be          | Food_Market_25.2.1     | 3-week | 2025-02-01 11:00:00 UTC | active |
       | added"                  | Food_Restaurant_25.2.1 | 4-week | 2025-02-21 11:00:00 UTC | future |
-
-  Scenario: Add several sprints using existing sprint prefixes
     When I successfully run `jira-auto-tool --sprint-add=25.3.1,4`
     Then afterwards the board only has the following sprints:
       | name                   | start_date              | state  |
@@ -34,6 +34,12 @@ Feature: Add sprints using existing ones as reference
       | Food_Restaurant_25.3.4 | 2025-06-13 11:00:00 UTC | future |
 
   Scenario: Add several planning interval sprints
+    Given the board only has the following sprints:
+      | comment                 | name                   | length | start_date              | state  |
+      | none added since closed | Food_Supply_25.1.3     | 2-week | 2025-02-01 11:00:00 UTC | closed |
+      | "sprints                | Food_Delivery_25.1.4   | 4-day  | 2025-02-01 11:00:00 UTC | future |
+      | expected to be          | Food_Market_25.2.1     | 3-week | 2025-02-01 11:00:00 UTC | active |
+      | added"                  | Food_Restaurant_25.2.1 | 4-week | 2025-02-21 11:00:00 UTC | future |
     When I successfully run `jira-auto-tool --sa=25.2.2,3  --sa=25.3.1,4  --sa=25.4.1,5`
     Then afterwards the board only has the following sprints:
       | name                   | start_date              | state  |
@@ -77,3 +83,25 @@ Feature: Add sprints using existing ones as reference
       | Food_Restaurant_25.4.3 | 2025-11-28 11:00:00 UTC | future |
       | Food_Restaurant_25.4.4 | 2025-12-26 11:00:00 UTC | future |
       | Food_Restaurant_25.4.5 | 2026-01-23 11:00:00 UTC | future |
+
+  Scenario: Adding sprints is not creating duplicates or ones anterior to last sprints of prefixes
+    Given the board only has the following sprints:
+      | comment                                              | name                   | length | start_date              | state  |
+      | none added since closed                              | Food_Supply_25.1.3     | 2-week | 2025-02-01 11:00:00 UTC | closed |
+      | all sprints added                                    | Food_Delivery_25.1.4   | 4-day  | 2025-02-01 11:00:00 UTC | future |
+      | the last 3 added since the first already exist       | Food_Market_25.2.1     | 3-week | 2025-02-01 11:00:00 UTC | active |
+      | none added because would be anterior existing sprint | Food_Restaurant_25.3.1 | 4-week | 2025-02-21 11:00:00 UTC | future |
+    When I successfully run `jira-auto-tool --sa=25.2.1,4`
+    Then afterwards the board only has the following sprints:
+      | name                   | start_date              | state  |
+      | Food_Supply_25.1.3     | 2025-02-01 11:00:00 UTC | closed |
+      | Food_Delivery_25.1.4   | 2025-02-01 11:00:00 UTC | future |
+      | Food_Delivery_25.2.1   | 2025-02-05 11:00:00 UTC | future |
+      | Food_Delivery_25.2.2   | 2025-02-09 11:00:00 UTC | future |
+      | Food_Delivery_25.2.3   | 2025-02-13 11:00:00 UTC | future |
+      | Food_Delivery_25.2.4   | 2025-02-17 11:00:00 UTC | future |
+      | Food_Market_25.2.1     | 2025-02-01 11:00:00 UTC | active |
+      | Food_Market_25.2.2     | 2025-02-22 11:00:00 UTC | future |
+      | Food_Market_25.2.3     | 2025-03-15 11:00:00 UTC | future |
+      | Food_Market_25.2.4     | 2025-04-05 11:00:00 UTC | future |
+      | Food_Restaurant_25.3.1 | 2025-02-21 11:00:00 UTC | future |
--- spec/jira/auto/tool/performer/planning_increment_sprint_creator_spec.rb
+++ spec/jira/auto/tool/performer/planning_increment_sprint_creator_spec.rb
@@ -33,27 +33,90 @@ module Jira
               end
             end
 
+            # rubocop:disable Naming/VariableNumber, RSpec/IndexedLet
             describe "#act_on_sprints_for_sprint_prefix" do
-              def get_sprint(name, attributes)
-                instance_double(Sprint, name: name, to_s: name, **attributes)
+              def get_sprint(name, attributes = {})
+                instance_double(Sprint, name: name, to_s: name, **attributes,
+                                        parsed_name: Sprint::Name.parse(name))
               end
 
-              let(:last_sprint) { get_sprint "Food_Delivery_25.2.1", end_date: "2025-02-16 12:45", length_in_days: 10 }
-
-              let(:sprint_prefix) { instance_double(Sprint::Prefix, name: "Food_Delivery", last_sprint: last_sprint) }
-
-              # rubocop:disable RSpec/MultipleExpectations
-              it "creates the expected number of sprints with the expected names" do
-                expect(updater).to receive(:create_sprint_for).ordered.with(last_sprint, "Food_Delivery_25.3.1")
-                expect(updater).to receive(:create_sprint_for).ordered.with(nil, "Food_Delivery_25.3.2")
-                expect(updater).to receive(:create_sprint_for).ordered.with(nil, "Food_Delivery_25.3.3")
-                expect(updater).to receive(:create_sprint_for).ordered.with(nil, "Food_Delivery_25.3.4")
-                expect(sprint_prefix).to receive(:<<).exactly(4).times
-
-                updater.act_on_sprints_for_sprint_prefix(sprint_prefix)
+              let(:sprint_prefix) do
+                instance_double(Sprint::Prefix, name: "Food_Delivery", last_sprint: last_sprint)
               end
-              # rubocop:enable RSpec/MultipleExpectations
+
+              context "when the sprints to create are all posterior to the last sprint from a naming perspective" do
+                let(:last_sprint) do
+                  get_sprint "Food_Delivery_25.2.1", end_date: "2025-02-16 12:45", length_in_days: 10
+                end
+
+                let(:sprint_25_3_1) { get_sprint("Food_Delivery_25.3.1") }
+                let(:sprint_25_3_2) { get_sprint("Food_Delivery_25.3.2") }
+                let(:sprint_25_3_3) { get_sprint("Food_Delivery_25.3.3") }
+                let(:sprint_25_3_4) { get_sprint("Food_Delivery_25.3.4") }
+
+                before do
+                  allow(updater).to receive(:create_sprint_for).with(last_sprint,
+                                                                     "Food_Delivery_25.3.1")
+                                                               .and_return(sprint_25_3_1)
+
+                  allow(updater).to receive(:create_sprint_for).with(sprint_25_3_1, "Food_Delivery_25.3.2")
+                                                               .and_return(sprint_25_3_2)
+
+                  allow(updater).to receive(:create_sprint_for).with(sprint_25_3_2, "Food_Delivery_25.3.3")
+                                                               .and_return(sprint_25_3_3)
+
+                  allow(updater).to receive(:create_sprint_for).with(sprint_25_3_3, "Food_Delivery_25.3.4")
+                                                               .and_return(sprint_25_3_4)
+                end
+
+                it "creates the expected number of sprints with the expected names" do
+                  expect(sprint_prefix).to receive(:<<).with(sprint_25_3_1)
+                  expect(sprint_prefix).to receive(:<<).with(sprint_25_3_2)
+                  expect(sprint_prefix).to receive(:<<).with(sprint_25_3_3)
+                  expect(sprint_prefix).to receive(:<<).with(sprint_25_3_4)
+
+                  updater.act_on_sprints_for_sprint_prefix(sprint_prefix)
+                end
+              end
+
+              context "when some sprints to create are anterior to the last sprint from a naming perspective" do
+                let(:last_sprint) do
+                  get_sprint "Food_Delivery_25.3.2", end_date: "2025-02-16 12:45", length_in_days: 10
+                end
+
+                let(:sprint_25_3_3) { get_sprint("Food_Delivery_25.3.3") }
+                let(:sprint_25_3_4) { get_sprint("Food_Delivery_25.3.4") }
+
+                before do
+                  allow(updater).to receive(:create_sprint_for).with(last_sprint, "Food_Delivery_25.3.3")
+                                                               .and_return(sprint_25_3_3)
+
+                  allow(updater).to receive(:create_sprint_for).with(sprint_25_3_3, "Food_Delivery_25.3.4")
+                                                               .and_return(sprint_25_3_4)
+                end
+
+                it "only creates the ones posterior to the last sprint" do
+                  expect(sprint_prefix).to receive(:<<).with(sprint_25_3_3)
+                  expect(sprint_prefix).to receive(:<<).with(sprint_25_3_4)
+
+                  updater.act_on_sprints_for_sprint_prefix(sprint_prefix)
+                end
+              end
+
+              context "when requested sprints to create are anterior to the last sprint from a naming perspective" do
+                let(:last_sprint) do
+                  get_sprint "Food_Delivery_25.4.1", end_date: "2025-02-16 12:45", length_in_days: 10
+                end
+
+                it "does not create any since they would be anterior to the last sprint" do
+                  expect(updater).not_to receive(:create_sprint_for)
+                  expect(sprint_prefix).not_to receive(:<<)
+
+                  updater.act_on_sprints_for_sprint_prefix(sprint_prefix)
+                end
+              end
             end
+            # rubocop:enable Naming/VariableNumber, RSpec/IndexedLet
           end
         end
       end
